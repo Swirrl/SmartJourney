ff9b111b96a937a7a0bf410fd018f3d7556a4e15
RicSwirrl <ric@swirrl.com>
2012-10-02 18:02:55 +0100

    precompile the data assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ PmdWinter::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( data.js )
+  config.assets.precompile += %w( data.css )
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
