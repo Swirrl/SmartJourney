d83cf24f069d8cd43ff785f448d9dcc9f7365913
RicSwirrl <ric@swirrl.com>
2012-11-08 17:30:48 +0000

    fix bug

--- app/controllers/publish_my_data/data_home_controller.rb
+++ app/controllers/publish_my_data/data_home_controller.rb
@@ -5,7 +5,7 @@ module PublishMyData
 
     def index
       datasets = Dataset.all
-      datasets.sort!{|a,b| dataset_order_comparison_operand(a) <=> dataset_order_comparison_operand(b) }
+      datasets.sort!{|a,b| a.title.downcase <=> b.title.downcase }
       @datasets = Kaminari.paginate_array(datasets).page(1).per(100)
     end
   end
