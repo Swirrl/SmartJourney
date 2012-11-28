82cf83176bbb956e6d7409e811d7f6235f9c5bf9
RicSwirrl <ric@swirrl.com>
2012-11-28 21:35:56 +0000

    fix tags route from ajax

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ PmdWinter::Application.routes.draw do
     match '/about' => "home#about"
     match '/help' => "home#help"
 
-    match '/reports/tags' => "reports#tags"
+    match '/reports/tags' => "reports#tags", :as => 'report_tags'
 
     resources :reports do
       member do
