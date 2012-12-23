bf162a66abac6ac7fbc26777ae23a9cee0544fd0
RicSwirrl <ric@swirrl.com>
2012-12-23 21:03:40 +0000

    remove redundant files

--- lib/report_update_alerts_job.rb
+++
@@ -1,8 +0,0 @@
-class ReportUpdateAlertsJob < Struct.new(:report_uri, :emails)
-  def perform
-    Rails.logger.info("Sending report update emails with delayed job")
-    emails.each do |email|
-      UserMailer.report_update_alert(report_uri, email).deliver
-    end
-  end
-end
